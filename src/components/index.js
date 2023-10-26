4baa497928214b24f60cbe579e2272fabbeb8f13
Jennovator <jenylltrinidadmabborang@gmail.com>
2023-10-26 18:32:55 -0400

    #Add: bare components

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import Hero from "./Hero";
+import NavBar from "./NavBar";
+import About from "./About";
+import Experience from "./Experience";
+import Projects from "./Projects";
+import Testimonials from "./Testimonials";
+import Contact from "./Contact";
+
+export {
+  Hero,
+  NavBar,
+  About,
+  Experience,
+  Projects,
+  Testimonials,
+  Contact
+};
