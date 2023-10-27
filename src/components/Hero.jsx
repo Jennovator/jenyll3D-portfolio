edbff270b13f88c5c3d7b9cbf4639022c90ad263
Jennovator <139597511+Jennovator@users.noreply.github.com>
2023-10-27 01:40:15 -0400

    #Update: added media queries for mobile and change style based on vp

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,6 +19,9 @@ const Hero = () => {
     return () => clearInterval(interval);
   }, [name]); // // Re-run the effect whenever the name changes
 
+   // Determine if the viewport width is less than or equal to 510 pixels
+   const isMobile = window.innerWidth <= 510;
+
   return (
     <section className="relative w-full h-screen mx-auto">
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl
@@ -29,7 +32,9 @@ const Hero = () => {
         </div>
 
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">{words[name]}</span></h1>
+          <h1 className={`${isMobile ? `${styles.sectionHeadText} text-white` : `${styles.heroHeadText}`}`}>
+            Hi, I'm <span className={`text-[#915eff] ${isMobile ? `${styles.sectionHeadText}` : ''}`}>
+              {words[name]}</span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop 3D visuals, user <br /> interfaces and web applications
           </p>
@@ -38,6 +43,21 @@ const Hero = () => {
 
       <ComputersCanvas />
       
+      <div className={`${isMobile ? `${styles.scrollMainAboutMobile}` : `${styles.scrollMainAboutPC}`}`}>
+        <a href="#about">
+        <div className={`${isMobile ? `${styles.scrollAboutMobile}` : `${styles.scrollAboutPC}`}`}>
+            <motion.div
+              animate={{ y: [0, 24, 0 ]}}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: 'loop'
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   )
 }
--- src/style.js
+++ src/style.js
@@ -12,6 +12,16 @@ const styles = {
       "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
     sectionSubText:
       "sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider",
+
+    scrollMainAboutPC:
+      "absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center",
+    scrollMainAboutMobile:
+      "absolute bottom-60 w-full flex justify-center items-center",
+
+    scrollAboutPC:
+      "w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2",
+    scrollAboutMobile:
+      "w-[30px] h-[50px] rounded-2xl border-4 border-secondary flex justify-center items-start p-1.5"
   };
   
   export { styles };
