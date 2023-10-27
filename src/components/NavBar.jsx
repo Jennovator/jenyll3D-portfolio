fe0075fb3ce11f049058060bed37e8071ffe036c
Jennovator <jenylltrinidadmabborang@gmail.com>
2023-10-26 21:14:00 -0400

    #Update: added navbar component functionality

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,77 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { styles } from '../style';
+import { navLinks } from '../constants';
+import { logo, menu, close } from '../assets';
 
 const NavBar = () => {
+
+  const [active, setActive] = useState(''); // for nav bar active state
+  const [toggle, setToggle] = useState(false); // for toggle menu
+
   return (
-    <div>NavBar</div>
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">Jenyll &nbsp;<span className="sm:block hidden">Mabborang</span></p>
+        </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${active === link.title
+                  ? "text-white"
+                  : "text-secondary"
+                } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+
+        {/* For Mobile Navigation bar*/}
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img
+            src={toggle ? close : menu}
+            alt="menu"
+            className="w-[28px] h-[28px] object-contain cursor-pointer"
+            onClick={() => setToggle(!toggle)}
+          />
+
+          <div className={`${!toggle ? 'hidden' : 'flex'} 
+            p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px]
+            z-10 rounded-xl`}>
+            <ul className="list-none flex justify-end items-start flex-col gap-4">
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${active === link.title
+                      ? "text-white"
+                      : "text-secondary"
+                    } font-poppins font-medium cursor-pointer text-[16px]`}
+                  onClick={() => {
+                    setToggle(!toggle);
+                    setActive(link.title);
+                  }}
+                >
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              ))}
+            </ul>
+
+          </div>
+        </div>
+      </div>
+    </nav>
   )
 }
 
--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ import Hero from "./Hero";
 import NavBar from "./NavBar";
 import About from "./About";
 import Experience from "./Experience";
+import Tech from "./Tech";
 import Projects from "./Projects";
 import Testimonials from "./Testimonials";
 import Contact from "./Contact";
@@ -11,6 +12,7 @@ export {
   NavBar,
   About,
   Experience,
+  Tech,
   Projects,
   Testimonials,
   Contact
