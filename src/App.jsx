659f05591eb089419cf9f53f80f73747f2dc4388
Jennovator <jenylltrinidadmabborang@gmail.com>
2023-10-26 20:44:06 -0400

    #Update: tailwind and components configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import { useState } from 'react'
+import { BrowserRouter } from "react-router-dom";
+import {  Hero, NavBar, About, Experience, Tech, Projects, Testimonials, Contact } from './components';
 
 const App = () => {
   return (
-      <div>
-        3D Portfolio
-      </div>
+      <BrowserRouter>
+        <div className="relative z-0 bg-primary">
+          <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+            <NavBar />
+            <Hero />
+          </div>
+          <About />
+          <Experience />
+          <Tech />
+          <Projects />
+          <Testimonials/>
+        
+          <div className="relative z-0">
+            <Contact />
+          </div>
+        </div>
+      </BrowserRouter>
   )
 }
 
