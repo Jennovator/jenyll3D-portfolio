cdbbdb00843fb33fdd9c30ef6b09241ccc3188c9
Jennovator <jenylltrinidadmabborang@gmail.com>
2023-10-30 22:04:35 -0400

    #Update: added email functionality

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,9 +16,42 @@ const Contact = () => {
 
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {}
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-  const handleSubmit = (e) => {}
+    setForm({ ...form, [name]: value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs.send(
+      'service_2xmw0al', 
+      'template_z0t8kje', 
+      {
+        from_name: form.name,
+        to_name: 'Jenyll Mabborang',
+        from_email: form.email,
+        to_email: 'jenyllmabborang@gmail.com',
+        message: form.message
+      },
+      'G6u1cIz_YBkii5oQl'
+    ).then(() =>{
+      setLoading(false);
+      alert('Thank you. I will get back to you as soon as possible.');
+
+      setForm({
+        name: '',
+        email: '',
+        message: ''
+      })
+    }, (error) => {
+      setLoading(false);
+      console.log(error);
+      alert('Something went wrong.');
+    })
+  }
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
